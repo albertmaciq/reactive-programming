004edf6f3ad965b101b6c7f3a44b03cdc3056119
albertmaciq <almaciq@gmail.com>
2023-04-18 09:28:34 +0200

    uploading init config project

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# reactive-programming
+# Spring Boot Reactor
+
+## Commands
+
+### Google-Java-Format
+
+```
+mvn com.coveo:fmt-maven-plugin:format
+mvn com.coveo:fmt-maven-plugin:check
+```
+
+### Checkstyle
+
+```
+mvn checkstyle:checkstyle
+```
+
+## References
+⚛️ [Project Reactor Dependency](https://projectreactor.io/docs/core/release/reference/#getting)
---
+++ src/main/java/com/springboot/reactor/SpringBootReactorApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.reactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactorApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(SpringBootReactorApplication.class, args);
+    }
+}
