5d9df83bc53ec9eb05c7002af2ec7475802af043
albertmaciq <almaciq@gmail.com>
2023-04-18 09:42:45 +0200

    subscribe method behaviour

--- src/main/java/com/springboot/reactor/SpringBootReactorApplication.java
+++ src/main/java/com/springboot/reactor/SpringBootReactorApplication.java
@@ -1,5 +1,7 @@
 package com.springboot.reactor;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,15 +11,22 @@ import reactor.core.publisher.Flux;
 @SpringBootApplication
 public class SpringBootReactorApplication implements CommandLineRunner {
 
+    private static final Logger log = LoggerFactory.getLogger(SpringBootReactorApplication.class);
+
     public static void main(final String[] args) {
         SpringApplication.run(SpringBootReactorApplication.class, args);
     }
 
     @Override
     public void run(final String... args) throws Exception {
-        Flux<String> names = Flux.just("Marcos", "Irene", "Tobias", "Sandra")
-            .doOnNext(System.out::println);
+        Flux<String> names = Flux.just("Marcos", "", "Irene", "Tobias", "Sandra")
+            .doOnNext(elem -> {
+                if (elem.isEmpty()) {
+                   throw new RuntimeException("Names can not be empty");
+                }
+                System.out.println(elem);
+            });
 
-        names.subscribe();
+        names.subscribe(log::info, error -> log.error(error.getMessage()));
     }
 }
